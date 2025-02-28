10a7ca3d1910a0350c81c33027aefcad6425200e
HPK017 <parelharsh0250@gmail.com>
2025-02-28 11:30:25 +0530

    user repository changes

---
+++ src/main/java/com/example/spring_jwt_auth/Repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.example.spring_jwt_auth.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.spring_jwt_auth.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    Optional<User> findByUsername(String username); 
+
+    Boolean existsByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    boolean existsByUsername(String username);
+    
+} 
