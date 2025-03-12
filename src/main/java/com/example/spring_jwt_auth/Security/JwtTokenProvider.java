5c5035d334e23dd87beed88673550d00b86af209
HPK017 <parelharsh0250@gmail.com>
2025-03-12 10:55:46 +0530

    validate token implementation

--- src/main/java/com/example/spring_jwt_auth/Security/JwtTokenProvider.java
+++ src/main/java/com/example/spring_jwt_auth/Security/JwtTokenProvider.java
@@ -2,22 +2,24 @@ package com.example.spring_jwt_auth.Security;
 
 import java.security.Key;
 import java.util.Date;
-import java.util.logging.Logger;
-
+import org.slf4j.Logger;   
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtTokenProvider {
 
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(JwtTokenProvider.class);
+    private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
 
     @Value("${app.jwt-secret}")
     private String jwtSecrert;
@@ -60,5 +62,23 @@ public class JwtTokenProvider {
         return username;
     }
 
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parse(token);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid Jwt token {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("Jwt tken is expired {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("Jwt Token is unsupported {}", e.getMessage() );
+        } catch (IllegalArgumentException e) {
+            logger.error("Jwt Claims string is empty {}", e.getMessage());
+        }
+        return false;
+    }
 
 }
